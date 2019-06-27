38a79c037fd985566cb5e7622b6e86be70c4a004
Kyubyong Park <kbpark.linguist@gmail.com>
2019-06-27 09:50:59 +0900

    Add files via upload

--- koparadigm/koparadigm.py
+++ koparadigm/koparadigm.py
@@ -14,19 +14,29 @@ class Paradigm(object):
             ret = []
             verb_labels = self.verb2labels[verb]
             for verb_label in verb_labels:
+                print(verb_label)
+                # pos
+                if verb_label < 27:
+                    pos = "동사"
+                elif verb_label in (27, 28):
+                    pos = "동사/형용사"
+                elif verb_label < 55:
+                    pos = "형용사"
+                else:
+                    pos = "조사"
+                # ending info.
                 ending2forms = OrderedDict()
                 rules = self.verb_label2rules[verb_label]
                 for ending_label, rule in rules:
                     endings = self.label2endings[ending_label]
                     for ending in endings:
                         forms = inflect(verb, ending, rule)
-                        if forms == []:
-                            forms = "N/A"
-                        else:
-                            forms = "/".join(forms)
+                        if len(forms)==0: continue
+                        forms = "/".join(forms)
 
                         ending2forms[ending] = forms
-                ret.append(ending2forms)
+
+                ret.append([pos, ending2forms])
             return ret
         else:
             print(f"{verb} is not in the dictionary.")
